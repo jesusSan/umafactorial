7ab1bac13c3b474d1566c600bf424fb7b18d058e
jesusSan <jesussanchezleal@gmail.com>
2017-03-13 13:28:01 +0100

    refactorizacion

--- umafactorial/src/umafactorial/Factorial.java
+++ umafactorial/src/umafactorial/Factorial.java
@@ -1,17 +1,22 @@
-package umafactorial;
-
 import java.util.Scanner;
 
 public class Factorial {
+	
+	public static long fact(long num) {
+		long fac = 1;
+		for (long i=1; i<=num; ++i) {
+			fac = fac * i;
+		}
+		return fac;
+	}
 	
 	public static void main(String []args) {
 		Scanner scanInput = new Scanner(System.in);
 		System.out.print("Introduzca un numero: ");
 		long num = scanInput.nextLong();
-		long fac = 1;
-		for (long i=1; i<=num; ++i) {
-			fac = fac * i;
-		}
+		
+		long fac = fact(num);
+		
 		System.out.println("El factorial de "+num+" es "+fac);
 		scanInput.close();
 	}
